62584b7a1d406c6219874b021d9b8f59414271c2
Avery Jones <averyjonesphoto+1@mac.com>
2022-06-10 23:07:56 +0000

    sneaky commit :)

--- LeetCode/104-MaximumDepthOfBinaryTree/maxDepth.py
+++ LeetCode/104-MaximumDepthOfBinaryTree/maxDepth.py
@@ -3,6 +3,7 @@
 #
 # Time O(n) - Faster than 83.40%
 # Space O(n) - Less than 22.40%
+
 def maxDepth(self, root: Optional[TreeNode]) -> int:
   if(root == None): return 0
   return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
